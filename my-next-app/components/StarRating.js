2f3818d850014a100f07d802b78b89e5a2b7a8af
Sarah Hackett <sarah.hackett@flutteruki.com>
2025-07-31 15:19:41 +0100

    Star rating

--- my-next-app/components/StarRating.js
+++ my-next-app/components/StarRating.js
@@ -1,21 +1,48 @@
+"use client";
 import styles from "./StarRating.module.css";
-import { FaRegStar } from "react-icons/fa";
-
+import { FaStar, FaRegStar } from "react-icons/fa";
+import { useState } from "react";
 
 const StarRating = () => { 
 
-    return (
+    const [rating, setRating] = useState(0);
+
+    const handleIncreaseRating = () => {
+        if (rating < 10) {
+          setRating(rating + 1);
+        }
+      };
+
+    const handleDecreaseRating = () => {
+        if (rating > 0) {
+          setRating(rating - 1);
+        }
+      };
+
+      return (
         <div className={styles.starRating}>
-            <div className={styles.starRatingContainer}>
-            <FaRegStar />
+          <div className={styles.starRatingContainer}>
 
-       
-            </div>
-
-            <button className={styles.button}>Click me</button>
-
+            {/* create a new empty array with 5 elements, map over it and return a FaStar component if the index 
+            is less than the rating, otherwise return a FaRegStar component */}
+            {[...Array(5)].map((_, index) =>
+              index < rating ? (
+                <FaStar key={index} className={styles.star} />
+              ) : (
+                <FaRegStar key={index} className={styles.star} />
+              )
+            )}
+          </div>
+    
+          <button className={styles.button} onClick={handleIncreaseRating}>
+            Rating Up
+          </button>
+          <button className={styles.button} onClick={handleDecreaseRating}>
+            Rating Down
+          </button>
+    
         </div>
-    )
-}
-
-export default StarRating;
+      );
+    };
+    
+    export default StarRating;
